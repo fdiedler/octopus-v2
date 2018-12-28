313742e3850541b0054640efbc4ac9335c06458a
florent <you@example.com>
2018-12-28 15:11:57 +0100

    ee

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: vendor/bin/heroku-php-nginx -C heroku/nginx_host.conf public/
--- features/bootstrap/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Symfony\Component\Dotenv\Dotenv;
-
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    (new Dotenv())->load(__DIR__.'/../../.env');
-}
